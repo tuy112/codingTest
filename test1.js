cd193f1f58c351ae1df48a7a33616d0c43207bf9
tuy112 <master_catch@naver.com>
2023-10-16 15:59:59 +0900

    feat:test01

---
+++ test1.js
@@ -0,0 +1,20 @@
+const a = 4;
+const b = 5;
+console.log("a =", a);
+console.log("b =", b);
+
+console.log("=============================================================");
+
+let d = "bibimbap";
+console.log(d);
+d = "bulgogi";
+console.log(d);
+
+console.log("=============================================================");
+
+console.log(name);
+console.log(product);
+console.log(price);
+var name = "jstol";
+const product = "table";
+let price = 500;
